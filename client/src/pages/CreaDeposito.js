424f687d39dfe0fe19f15cd7c85cf10067934141
Lord virus <illia-romaniuk@outlook.it>
2020-06-01 12:46:32 +0200

    style deposito finale

--- client/src/pages/CreaDeposito.js
+++ client/src/pages/CreaDeposito.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "../components/Nav/Navbar";
 import Rete from "../pages/componenti_deposito/rete";
 import InputD from "../pages/componenti_deposito/input_deposito";
+import Invia from "../pages/componenti_deposito/invia";
 
 class CreaDeposito extends Component {
   render() {
@@ -12,6 +13,7 @@ class CreaDeposito extends Component {
         <Navbar showMe={true}>Deposito</Navbar>
         <Rete rete={rete} balance={balance} />
         <InputD />
+        <Invia />
       </div>
     );
   }
