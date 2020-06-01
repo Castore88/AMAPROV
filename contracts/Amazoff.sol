5d4081284ac648bdb66599ac83a095b30a0b1bb6
Άρτεμις <Artemis@MacBook-di-Freia.local>
2020-06-01 19:20:15 +0200

    contratto iniziale

--- contracts/Amazoff.sol
+++ contracts/Amazoff.sol
@@ -5,7 +5,6 @@ contract Amazoff {
     address owner;
     mapping(address => uint256) deposits;
     uint256 public blackFridayEndDate;
-    uint256 bal;
 
     modifier byOwner {
         require(msg.sender == owner, "Not allowed");
@@ -19,26 +18,20 @@ contract Amazoff {
 
     constructor() public {
         owner = msg.sender;
-        bal = 0;
-    }
-
-    function getBalance() public view returns (uint256) {
-        return bal;
     }
 
     function setBlackFriday(uint256 timestamp) public ifBlackFridayClosed {
         blackFridayEndDate = timestamp;
     }
 
-    function deposit(uint256 amt) public payable {
+    function deposit() public payable {
         deposits[msg.sender] = msg.value;
-        bal = bal + amt;
     }
 
-    function withdraw(uint256 amt) public ifBlackFridayClosed {
+    function withdraw() public ifBlackFridayClosed {
+        require(deposits[msg.sender] > 0, "No more money");
         uint256 value = deposits[msg.sender];
         deposits[msg.sender] = 0;
         msg.sender.transfer(value);
-        bal = bal - amt;
     }
 }
